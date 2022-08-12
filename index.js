3d4308ac8722d7726397d1884126f1a73649eedc
Phaedrus <92445005+ashton8504@users.noreply.github.com>
2022-08-12 11:26:01 -0400

    Update index.js
    Refactored Code

--- index.js
+++ index.js
@@ -18,37 +18,27 @@ const poundToKilo = 0.453
 convertBtn.addEventListener("click", function() {
     let baseValue = inputEl.value
     
-    lengthEl.textContent = `
-                            ${baseValue} Meters = ${(baseValue * meterToFeet).toFixed(3)} Feet 
-                                ||
-                            ${baseValue} Feet = ${(baseValue * feetToMeter).toFixed(3)} Meters 
-                           ` 
-})
-
-//For the Volume Converter//
-convertBtn.addEventListener("click", function () {
-    let baseValue = inputEl.value
-    
-    volumeEl.textContent = `
-                            ${baseValue} Liters = ${(baseValue * literToGallon).toFixed(3)} Gallons 
-                                ||
-                            ${baseValue} Gallons = ${(baseValue * gallonToLiter).toFixed(3)} Liters
-                           `
-})
-
-//For the Mass Converter//
-convertBtn.addEventListener('click', function () {
-    let baseValue = inputEl.value
-    
-    massEl.textContent = `
-                            ${baseValue} Kilos = ${(baseValue * kiloToPound).toFixed(3)} Pounds 
-                                ||
-                            ${baseValue} Pounds = ${(baseValue * poundToKilo).toFixed(3)} Kilos
-                         `
+    lengthEl.textContent = 
+        `
+            ${baseValue} Meters = ${(baseValue * meterToFeet).toFixed(3)} Feet 
+                ||
+            ${baseValue} Feet = ${(baseValue * feetToMeter).toFixed(3)} Meters 
+        ` 
+    volumeEl.textContent = 
+        `
+            ${baseValue} Liters = ${(baseValue * literToGallon).toFixed(3)} Gallons 
+                ||
+            ${baseValue} Gallons = ${(baseValue * gallonToLiter).toFixed(3)} Liters
+        `
+    massEl.textContent = 
+        `
+            ${baseValue} Kilos = ${(baseValue * kiloToPound).toFixed(3)} Pounds 
+                ||
+            ${baseValue} Pounds = ${(baseValue * poundToKilo).toFixed(3)} Kilos
+        `
 })
 
 resetBtn.addEventListener('click', function () {
     
     inputEl.value = "0"
 })
- 
